b183ff3986f94dda5c5ae73a48d60295dd849b51
Sanath <sanath.official.1998@gmail.com>
2024-08-30 19:37:18 +0530

    terraform config setup

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+    description = "Value of the AWS region"
+    type        = string
+    default     = "ap-south-1"
+}
+
+variable "aws_access_key" {
+    description = "Value of the AWS Access key"
+    type        = string
+    default     = "AKIATG6MGGYGWO6MS2SQ"
+}
+
+variable "aws_secret_key" {
+    description = "Value of the AWS Secret key"
+    type        = string
+    default     = "vr27WBFzoRWE888g9U0/ikvHVZ4UJ5y0HXztUmm0"
+}
+
